0763b774963ccf4c0514153686bb467b642c1cad
Pierre Schumacher <pierre.schumacher@tuebingen.mpg.de>
2023-04-27 14:57:49 +0200

    fixed play_files for newest updates

--- play_files/play_dep_dmcontrol_quadruped.sh
+++ play_files/play_dep_dmcontrol_quadruped.sh
@@ -1,5 +1,5 @@
 python -m deprl.play\
-	--header 'import tonic.torch; from deprl import custom_agents; custom_agents;import gym;import ostrichrl;'\
-	--env "tonic.environments.ControlSuite('quadruped-run', scaled_actions=False)" \
-	--agent 'custom_agents.dep_factory(4, tonic.torch.agents.MPO())(replay=tonic.replays.buffers.Buffer(return_steps=3, batch_size=256, steps_between_batches=1000, batch_iterations=30, steps_before_batches=1000e4))' \
+	--header 'import deprl, gym'\
+	--env "deprl.environments.ControlSuite('quadruped-run', scaled_actions=False)" \
+	--agent 'deprl.custom_agents.dep_factory(4, deprl.torch.agents.MPO())(replay=deprl.replays.buffers.Buffer(return_steps=3, batch_size=256, steps_between_batches=1000, batch_iterations=30, steps_before_batches=1000e4))' \
 	--seed 0
