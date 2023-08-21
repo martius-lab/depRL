cc764f04e6f08ec3b0149775d5393a34ffbd67ad
Pierre <pierre.schumacher@tuebingen.mpg.de>
2023-08-21 09:58:41 +0200

    added exception test

--- tests/test_deprl.py
+++ tests/test_deprl.py
@@ -2,8 +2,12 @@ import shutil
 import sys
 
 import gym
+from deprl.vendor.tonic import logger
 import myosuite  # noqa
 import torch
+from myosuite.envs.myo.walk_v0 import WalkEnvV0
+from myosuite.envs import myo
+
 
 import deprl
 from deprl import main, play
@@ -38,6 +42,36 @@ def test_train():
     shutil.rmtree("./tests/test_DEPRL", ignore_errors=True)
 
 
+def test_exception():
+    class TestException(Exception):
+        pass
+    class ExceptionTester(gym.Wrapper):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.exception_steps = 0
+            self.exception_limit = 5e2
+
+        def step(self, action):
+            self.exception_steps += 1
+            if not self.exception_steps % self.exception_limit:
+                raise TestException('Artificial Exception initiated')
+            return self.env.step(action)
+
+    env = ExceptionTester(gym.make('myoLegWalk-v0'))
+    env.reset()
+    for i in range(1000):
+        try:
+            env.step(env.action_space.sample())
+        except TestException as e:
+            logger.log(f'TestError is: {e}')
+
+
+
+
+
+
 if __name__ == "__main__":
+
+    test_exception()
     test_train()
     test_play()
