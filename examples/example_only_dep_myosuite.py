3e35827a573648a6a11079491ba7a4feb06b5ce2
Pierre Schumacher <pierre.schumacher@tuebingen.mpg.de>
2023-12-21 16:05:06 +0100

    precommit hooks

--- examples/example_only_dep_myosuite.py
+++ examples/example_only_dep_myosuite.py
@@ -1,25 +1,20 @@
-import gym
-import myosuite
 import time
+
+import gym
+import myosuite  # noqa
+
 from deprl import env_wrappers
 from deprl.dep_controller import DEP
 
-
-
-env = gym.make('myoLegWalk-v0')
+env = gym.make("myoLegWalk-v0")
 env = env_wrappers.GymWrapper(env)
 
-# You can also use SconeWrapper for Wrapper
-# env = env_wrappers.SconeWrapper(env)
 dep = DEP()
 dep.initialize(env.observation_space, env.action_space)
 
 env.reset()
 for i in range(1000):
-    action = dep.step(env.muscle_lengths())[0,:]
-    print(action.shape)
+    action = dep.step(env.muscle_lengths())[0, :]
     next_state, reward, done, _ = env.step(action)
-    time.sleep(0.01)
+    time.sleep(0.005)
     env.mj_render()
-
-
