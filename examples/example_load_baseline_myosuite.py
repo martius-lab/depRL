2217d0e176609688fb711e3950544271783d3197
Pierre Schumacher <schumacherpier@gmail.com>
2024-07-10 18:40:31 -0400

    fixed myosuite baseline example for gymnasium

--- examples/example_load_baseline_myosuite.py
+++ examples/example_load_baseline_myosuite.py
@@ -3,26 +3,25 @@
 
 import time
 
-import gym
 import myosuite  # noqa
+from myosuite.utils import gym
 
 import deprl
 
-# create the sconegym env
-env = gym.make("myoChallengeChaseTagP1-v0")
+env = gym.make("myoLegWalk-v0", reset_type="random")
 policy = deprl.load_baseline(env)
 
 env.seed(0)
 for ep in range(10):
     ep_steps = 0
     ep_tot_reward = 0
-    state = env.reset()
+    state, _ = env.reset()
 
     while True:
         # samples random action
         action = policy(state)
         # applies action and advances environment by one step
-        state, reward, done, info = env.step(action)
+        state, reward, terminated, truncated, info = env.step(action)
 
         ep_steps += 1
         ep_tot_reward += reward
@@ -30,11 +29,10 @@ for ep in range(10):
         time.sleep(0.01)
 
         # check if done
+        done = terminated or truncated
         if done or (ep_steps >= 1000):
             print(
                 f"Episode {ep} ending; steps={ep_steps}; reward={ep_tot_reward:0.3f};"
             )
             env.reset()
             break
-
-env.close()
